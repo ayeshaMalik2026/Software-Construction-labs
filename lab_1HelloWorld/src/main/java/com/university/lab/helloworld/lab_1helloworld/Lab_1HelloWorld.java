4a107a0723d43dff42f1d81fa79de0573c094eb4
Ayesha Malik <24abswe0014@uetpeshawar.edu.pk>
2026-08-20 21:04:39 +0500

    Initial Lab 1 commit

---
+++ lab_1HelloWorld/src/main/java/com/university/lab/helloworld/lab_1helloworld/Lab_1HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.university.lab.helloworld.lab_1helloworld;
+
+/**
+ *
+ * @author ayesh
+ */
+public class Lab_1HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
